5cb4a1e652bad294cca36ec73443ae69b6257006
Vikash Khatri <102739330+VikashKhatri95@users.noreply.github.com>
2024-07-23 04:50:59 +0000

    Used Function

--- .devcontainer/PracticeQuestions/Campusx_Q2.py
+++ .devcontainer/PracticeQuestions/Campusx_Q2.py
@@ -1,4 +1,8 @@
 #Program that will convert celisus into fahrenheit.
+def converter(temp):
+    convertedTemp=(temp*5)/2+32
+    return convertedTemp
+
 temperature_inc=float(input("Enter Temperature in Celsius"))
-temperature_inf=(temperature_inc*5)/2+32
-print(temperature_inf)
+temperature_inf=converter(temperature_inc)
+print(temperature_inf,type(temperature_inf))
