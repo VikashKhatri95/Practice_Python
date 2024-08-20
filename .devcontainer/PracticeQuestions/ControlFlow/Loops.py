a96995283d836aa72c955d091c3655b0e250ef80
Vikash Khatri <102739330+VikashKhatri95@users.noreply.github.com>
2024-08-20 12:14:18 +0000

    Udemy2

--- .devcontainer/PracticeQuestions/ControlFlow/Loops.py
+++ .devcontainer/PracticeQuestions/ControlFlow/Loops.py
@@ -4,3 +4,20 @@ for i in range(5):
 
 for i in range(5,20,2): #start,end,steps
     print(i)
+
+for i in range(5,20,-1): #start,end,steps
+    print(i)     #nothing will be printed
+
+for i in range(10,1,-1):
+    print(i)    #10,9,8...,3,2
+
+#string is a collection of characters
+str="Vikash Khatri"
+for i in str:
+    print(i)
+
+#while loop continues to execute a piece of code untill the condition becoms false.
+count=1
+while count<5:
+    print(count)
+    count=count+1
---
+++ .devcontainer/PracticeQuestions/ControlFlow/loopControl.py
@@ -0,0 +1,15 @@
+#loop control
+#break statement exits in loop permanently
+
+for i in range(5):
+    print(i)
+    if i ==3:
+        break
+#continue skips the current iteration and continues with the next.
+print()
+for i in range(10):
+    if i%2==0:
+        continue
+    print(i)
+
+#pass statement is a null statement that does nothing
